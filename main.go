8d1f5690e7d03ae5721d37b4702cdb5538c577ff
gongna <2036479155@qq.com>
2024-07-31 17:28:49 +0800

    feat:not check err

--- main.go
+++ main.go
@@ -5,19 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!") // 这行代码是有效的
 
-	// 下面的代码将引发 linting 问题
-	var unusedVariable int // 未使用的变量
-	fmt.Println(unusedVariable)
-
 	// 下面的代码将引发错误
 	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+		i = 1
+
 	}
 
 	// 这行代码将引发一个潜在的错误
 	// 这里的错误是因为没有处理错误
-	_, err := fmt.Println("This will not be checked") // 忽略错误
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	var err error
+	_, err = fmt.Println("This will not be checked") // 忽略错误
+	fmt.Println(err)
+	_, err = fmt.Println("This will not be checked") // 忽略错误
+
 }
